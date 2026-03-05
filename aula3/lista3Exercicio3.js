adbeb59e32fd102b3bdc72a82a9e3189546a8148
ViniciosBrandao <vhfbrandao@gmail.com>
2026-03-05 09:38:41 -0300

    atualizando para melhor lidar com duas casas decimais

--- aula3/lista3Exercicio3.js
+++ aula3/lista3Exercicio3.js
@@ -7,7 +7,7 @@ const meses = Number(prompt("Por quanto meses? "));
 
 // Processamento
 const valorAcumulado = ((1 + taxa) ** meses - 1) * aplicacaoMensal / taxa;
-const valorFinal = Math.round(valorAcumulado * 100) / 100; // Move a virgula, arrendoda e depois volta para duas casas decimais.
+
 
 // Saída da Informação
-console.log(`O valor acumulado para uma aplicação de ${aplicacaoMensal} reais mensal para ${meses} mese(s) com uma taxa de ${taxaInput}% é igual a R$ ${valorFinal} reais`);
+console.log(`O valor acumulado para uma aplicação de ${aplicacaoMensal} reais mensal para ${meses} mese(s) com uma taxa de ${taxaInput}% é igual a R$ ${valorAcumulado.toFixed(2)} reais`);
