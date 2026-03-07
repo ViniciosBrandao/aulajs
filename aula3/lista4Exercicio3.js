110462662f4aad7e6bb44c44f2a2acb9b06a44e4
ViniciosBrandao <vhfbrandao@gmail.com>
2026-03-07 09:07:21 -0300

    Exercício que calcula o custo de um cilindro

--- aula3/lista4Exercicio3.js
+++ aula3/lista4Exercicio3.js
@@ -1,14 +1,14 @@
 // Entrada de dados
 const prompt = require("prompt-sync")();
-const raio = Number(prompt("Qual o raio da lata? "));
+const raio = Number(prompt("Qual o raio da lata em metros? "));
 const altura = Number(prompt("Qual a altura da lata em metros? "));
 const custo = 155;
-const arquibancada = 30 / 100 * publico * 20;
-const cadeiras = 10 / 100 * publico * 40;
 
 // Processamento
-const areaLata = (Math.PI * raio ** 2 * 2)
-const valorTotal = popular + geral + arquibancada + cadeiras
+const areaBase = (Math.PI * raio ** 2 * 2);
+const areaLado = (2 * Math.PI * raio * altura);
+const areaLata = areaBase + areaLado
+const valorTotal = areaLata * custo
 
 // Saída da Informação
-console.log(`O valor da renda total do jogo é de R$ ${valorTotal.toFixed(2)} reais`);
+console.log(`O valor total da lata é de R$ ${valorTotal.toFixed(2)} reais`);
