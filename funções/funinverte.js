4ef21e1bc4328685070cbc5e1e3508ee386bd420
ViniciosBrandao <vhfbrandao@gmail.com>
2026-03-17 14:10:49 -0300

    adiciona comentários explicativos

--- funções/funinverte.js
+++ funções/funinverte.js
@@ -1,10 +1,10 @@
 function inverterNumero(valor){
-    let numeroI = valor % 10;
-    valor = Math.trunc(valor / 10);
-    numeroI = numeroI * 10 + valor % 10;
-    valor = Math.trunc(valor / 10);
-    numeroI = numeroI * 10 + valor;
-    return numeroI
+    let numeroI = valor % 10; // extrai último dígito. Ex: 123> 3
+    valor = Math.trunc(valor / 10); // remove o último dígito. trunc remove a parte decimal. 123> 12
+    numeroI = numeroI * 10 + valor % 10; // desloca o dígito extraído e adiciona o próximo. 3 * 10 + 12 % 10 = 32
+    valor = Math.trunc(valor / 10); // remove o último dígito. 12 /10 = 1
+    numeroI = numeroI * 10 + valor; // adiciona o último dígito, recompondo os 3 dígitos em forma invertida.
+    return numeroI // retorna o número invertido
 }
 
 // Entrada de Dados
