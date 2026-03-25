64436911db936a617b354e9d50a96d939b2f2df4
ViniciosBrandao <vhfbrandao@gmail.com>
2026-03-25 14:15:03 -0300

    testes utilizando operadores relacionais e lógicos

---
+++ fundamentos/relacionaisTestes.js
@@ -0,0 +1,5 @@
+if ("casa" > "cachorro") console.log("casa é maior que cachorro, pois s vem depois d c");
+if ("a" < "b") console.log("b é maior que a porque vem depois da a na tabela unicode");
+if ("Z" < "a") console.log("Maiscula sao Menores, vem antes na tabela unicode");
+if (100 > "20") console.log("Aqui tem coerção de tipo, a string é tipada em number e entao comparado")
+if ("100" < "20") console.log("Por ambos serem string, compara-se o primeiro caractere, logo, 2 > 1");
