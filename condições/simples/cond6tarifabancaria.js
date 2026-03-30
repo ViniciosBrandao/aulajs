2bd5691d8a1eef0b064b294ba14942677b70e1c1
ViniciosBrandao <vhfbrandao@gmail.com>
2026-03-30 08:50:38 -0300

    refactor: simplifica lógica de classificação de tarifa usando intervalos progressivos

--- condições/simples/cond6tarifabancaria.js
+++ condições/simples/cond6tarifabancaria.js
@@ -14,21 +14,24 @@ Obs.: Usuário ideal - ele não irá digitar valores incorretos.
 */
 
 function calcularTarifaBancaria(saldoBancario){
+
     const saldoMedioMensal = saldoBancario / 6;
-    if (saldoMedioMensal <= 1000) return "Básica (saldo médio mensal inferior a R$ 1.000,00) tarifa de R$25,00 reais";
-    if (saldoMedioMensal > 1000 && saldoMedioMensal <= 2000) return "Prata (saldo médio mensal entre R$ 1.000,01 e R$ 2.000,00) tarifa de R$ 20,00";
-    if (saldoMedioMensal > 2000 && saldoMedioMensal <= 3500) return "Ouro (saldo médio mensal entre R$ 2.000,01 e R$ 3.500,00)tarifa de R$ 13,00";
-    if (saldoMedioMensal > 3500) return "Prêmio (saldo médio mensal superior a R$ 3.500,00) tarifa isenta.";
+
+    if (saldoMedioMensal <= 1000) return "Básica - R$25,00";
+    if (saldoMedioMensal <= 2000) return "Prata - R$20,00";
+    if (saldoMedioMensal <= 3500) return "Ouro - R$13,00";
+
+    return "Prêmio - isento";
 }
 
-// Entrada de dados
+// Entrada
 const prompt = require("prompt-sync")();
 const nome = prompt("Nome: ");
 const numeroConta = Number(prompt("Número da conta: "));
 const saldoBancario = Number(prompt("Saldo bancário semestral: "));
 
-// Processamento  
-const resposta = calcularTarifaBancaria(saldoBancario);
+// Processamento
+const tarifa = calcularTarifaBancaria(saldoBancario);
 
-// Saída da Informação
-console.log(`${nome} dono da conta número ${numeroConta} tem tarifa: ${resposta}`);
+// Saída
+console.log(`${nome} (conta ${numeroConta}) possui tarifa: ${tarifa}`);
