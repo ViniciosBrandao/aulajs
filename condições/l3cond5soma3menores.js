c1d6e9c27532f7d7fb6c3db6620cbbfb76fb9b6f
ViniciosBrandao <vhfbrandao@gmail.com>
2026-04-13 09:33:30 -0300

    feat: soma três numeros menores com condicional aninhada

---
+++ condições/l3cond5soma3menores.js
@@ -0,0 +1,34 @@
+/*
+Dados quatro números distintos, desenvolver um programa que
+determine e imprima a soma dos três menores.
+*/
+function somar3Menores(numero1, numero2, numero3, numero4) {
+    if (numero1 < numero2 || numero1 < numero3 || numero1 < numero4) {
+        if (numero1 > numero2 || numero2 < numero3 || numero2 < numero4) {
+            if (numero3 < numero4) {
+                return numero1 + numero2 + numero3;
+            } else {
+                return numero1 + numero2 + numero4;
+            }
+        } else {
+            return numero1 + numero3 + numero4;
+        }
+    } else {
+        return numero2 + numero3 + numero4;
+    }
+}
+
+// Entrada de Dados
+const prompt = require("prompt-sync")();
+
+// Processamento e Saída da Informação
+let opcao = "";
+do{
+    const numero1 = Number(prompt("Número 1: "));
+    const numero2 = Number(prompt("Número 2: "));
+    const numero3 = Number(prompt("Número 3: "));
+    const numero4 = Number(prompt("Número 4: "));
+    const resposta = somar3Menores(numero1, numero2, numero3, numero4);
+    console.log(`A soma dos três menores é: ${resposta}`);
+    opcao = prompt("Deseja continuar? (S/N) ");
+} while (opcao.toUpperCase() === "S");
