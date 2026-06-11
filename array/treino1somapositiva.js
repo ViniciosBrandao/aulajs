851a19e59b3638f4d8fd602fcf4b925fb3d95c11
ViniciosBrandao <vhfbrandao@gmail.com>
2026-06-11 16:18:13 -0300

    feat: adiciona teste e função imperativo e declarativo superior eficiente

--- array/treino1somapositiva.js
+++ array/treino1somapositiva.js
@@ -16,6 +16,17 @@ Note
 If there is nothing to sum, the sum is default to 0.
 
 */
+// Entrada de Dados
+const arr = [-1, 10, 25, 0, 2];
+
+function somaPositivaArray(arr) {
+    let total = 0;
+
+    for (let elemento = 0; elemento < arr.length; elemento++) {
+        if (arr[elemento] > 0) total += arr[elemento];
+    }
+    return total;
+}
 function positiveSum(arr) { // mais legível, primeiro filter depois reduce
   const positivos = arr.filter(n => n > 0);
 
@@ -24,4 +35,7 @@ function positiveSum(arr) { // mais legível, primeiro filter depois reduce
 function positiveSumEficiente(arr) { // apenas um array, reduce com condicional
     return arr.reduce((acc, n) => n > 0 ? acc + n : acc, 0);
 }
-
+// Processamento e Saída da Informação
+console.log(somaPositivaArray(arr));
+console.log(positiveSumEficiente(arr));
+console.log(positiveSumEficiente(arr));
