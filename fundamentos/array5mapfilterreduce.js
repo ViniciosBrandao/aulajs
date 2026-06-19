4c59ece1a88b7d310ac8bd3d962ebb1511d876c4
ViniciosBrandao <vhfbrandao@gmail.com>
2026-06-19 09:47:59 -0300

    atualiza formatação dos comentários

--- fundamentos/array5mapfilterreduce.js
+++ fundamentos/array5mapfilterreduce.js
@@ -1,5 +1,5 @@
 const numeros = [1, 2, 3, 4, 5];
-const dobro = numeros.map(n => n * 2); // O que acontece: Para CADA elemento do array Aplique uma função Crie um novo array com os resultados
+const dobro = numeros.map(n => n * 2); // O que acontece: Para CADA elemento do array Aplique uma funçã. Crie um novo array com os resultados
 const pares = dobro.filter(n => n % 2 === 0); // O que acontece: Para CADA elemento Teste uma condição (verdadeiro/falso) Se verdadeiro, inclua no novo array
 const soma = pares.reduce((acumulador, n) => acumulador + n, 0); // O valorInicial (0 no exemplo) é o ponto de partida. Mentalidade: "Quero 'colapsar' todos os elementos em um único valor."
 // arr.reduce((primeiroParametro, segundoParametro, terceiroParametro, quartoParametro) => {...}, valorInicial)
