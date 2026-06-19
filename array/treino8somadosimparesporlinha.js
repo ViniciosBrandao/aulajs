43a8a28cd8dedddb57d518c867e1ac6951a143d3
ViniciosBrandao <vhfbrandao@gmail.com>
2026-06-19 18:15:52 -0300

    adicionar comentário com explicação matemática

--- array/treino8somadosimparesporlinha.js
+++ array/treino8somadosimparesporlinha.js
@@ -12,7 +12,7 @@ Calculate the sum of the numbers in the nth row of this triangle (starting at in
 1 -->  1
 2 --> 3 + 5 = 8
 */
-function rowSumOddNumbers(n) { // minha primeira tentativa
+function rowSumOddNumbers2(n) { // minha primeira tentativa
     let soma = 0;
     let termo = 1;
     let arr = [];
@@ -39,6 +39,20 @@ function rowSumOddNumbers(n) { // versao otimizada, utilizando progressao aritm√
     return (quantidadeNaLinha * (primeiroDaLinha + ultimoDaLinha)) / 2; // soma de PA
 }
 function rowSumOddNumbers(n) { // padrao matematico da soma das linhas dos impares, mais eficiente poss√≠vel.
+    /* The rows' start numbers are Hogben's centered polygonal numbers:
+     1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+     <https://oeis.org/A002061>
+     
+     The sum of one row is given by:
+     s[n] = n^2 + n(b[n] - 1).
+     <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
+     
+     Inline b[n]:
+     s[n] = n^2 + n(n^2 - n + 1 - 1)
+          = n^2 + n(n^2 - n)
+          = n^2 + n^3 - n^2
+          = n^3
+     ... oh. */
     return n ** 3;
 }
 
