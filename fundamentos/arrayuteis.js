ef9210f4adf2b61aa32ec5b510292c7c39bfc4ce
ViniciosBrandao <vhfbrandao@gmail.com>
2026-07-08 10:29:25 -0300

    adiciona shift, pop, reverse

--- fundamentos/arrayuteis.js
+++ fundamentos/arrayuteis.js
@@ -24,3 +24,16 @@ array.forEach(fn)     // como map mas não retorna nada — para efeitos colater
 // CONVERSÃO
 Array.from("abc")     // cria array de qualquer iterável → ["a","b","c"]
 Array.isArray(valor)  // verifica se é array — retorna true/false
+
+// Remove o primeiro elemento e Retorna  
+array.shift() // Mais lento que pop pois precisa reorganizar os indices do array, importa para arrays grandes. Muta o array original. se array for vazio retorna undefined
+
+// Remove o ultimo elemento e Retorna
+array.pop()  // mais barato que shift porque não precisa reindexar — o último elemento simplesmente some, sem deslocar ninguém. se array vazio retorna undefined.
+
+// Inverte a ordem dos elementos do array no próprio lugar e retorna o mesmo array modificado.
+array.reverse() // Muta o original. devolve o próprio original modificado, não uma cópia. se precisar de preservar original [...original].reverse()
+
+// Retorna um novo array que é a junção do array original com os valores passados como argumento. O original não é tocado.
+a.concat(b) // junta os elementos de a e b em um novo array. Nao muta os arrays originais
+[1, 2].concat([3, 4], 5, [6]); // [1, 2, 3, 4, 5, 6] Essa diferença é importante: se você fizer result.push([1,2,3]), o resultado fica [..., [1,2,3]] — um array dentro do array. concat([1,2,3]) distribui os elementos: [..., 1, 2, 3].
